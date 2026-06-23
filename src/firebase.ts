b65784ecbc1bdccf253a5e55d3342bbf0730c164
Phan Hai Anh <haianh2504077@gmail.com>
2026-06-23 07:58:36 +0700

    chỉnh sửa hardcode thành configurable ở .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDweO5_jPh7ainTlYCMahfMVunmDxGtz20",
-  authDomain: "love-date-29288.firebaseapp.com",
-  projectId: "love-date-29288",
-  storageBucket: "love-date-29288.firebasestorage.app",
-  messagingSenderId: "1063802009471",
-  appId: "1:1063802009471:web:475b83f568233af755cc4a",
-  measurementId: "G-4RZMSM5HT0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
